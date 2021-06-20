73dde175ba1dd8505fc009e8a351ff1108680829
Sandorik00 <sasha.kobec00@gmail.com>
2021-06-20 03:26:27 +0300

    add global types

---
+++ src/global.d.ts
@@ -0,0 +1,29 @@
+export type TChannel = {
+    id: number,
+    name: string,
+    type: string,
+    position: number,
+    category_id: string | null,
+    topic: string | null,
+    nsfw: boolean | null
+}
+
+export type TMessage = {
+    id: number,
+    channelID: string,
+    authorID: string,
+    messageID: string,
+    content: string | null,
+    embeds: string | null,
+    attachments: string | null,
+    reference: string | null,
+    created_at: string
+}
+
+export type TMember = {
+    id: number,
+    discordID: string,
+    name: string,
+    bot: boolean,
+    avatar_url: string
+}
